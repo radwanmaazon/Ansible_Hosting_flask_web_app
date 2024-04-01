11132b722e6c32a8c8801defc75c4d82df058431
RadwanMaazon <redwan30503321@gmail.com>
2024-04-01 23:10:46 +0200

    last update

--- App/Code/files/simple-flaskApp/backend/sql_connection.py
+++ App/Code/files/simple-flaskApp/backend/sql_connection.py
@@ -9,15 +9,8 @@ def get_sql_connection():
         __mydb = mysql.connector.connect(
             host="db1.mydomain.org",
             user="appuser",
-            password="appuser",
+            password="appuser" ,
             database="gs"
         )
-
-        # __mydb = mysql.connector.connect(
-        #     host="localhost",
-        #     user="radwan",
-        #     password="rDI@rdi123",
-        #     database="gs"
-        # )
-
+        
     return __mydb
