08b98da7c669bb36976eae6e3329c6a6022df054
RadwanMaazon <redwan30503321@gmail.com>
2024-05-03 23:17:19 +0300

    Code for Application and it configuration

--- App/Code/files/simple-flaskApp/backend/sql_connection.py
+++ App/Code/files/simple-flaskApp/backend/sql_connection.py
@@ -2,17 +2,22 @@ import configparser
 import mysql.connector
 
 __mydb = None
+
 def get_sql_connection():
     global __mydb
 
     if __mydb is None:
+        config = configparser.ConfigParser()
+        config.read('config.ini')
 
-        __mydb = mysql.connector.connect(
-            host="192.168.1.74",
-            port="6033",
-            user="playgrounduser",
-            password="{{ playgrounduser_pass }}",
-            database="gs"
-        )
-        
+        db_config = {
+            'host': config['database']['host'],
+            'port': config['database']['port'],
+            'user': config['database']['user'],
+            'password': config['database']['password'],
+            'database': config['database']['name']
+        }
+
+        __mydb = mysql.connector.connect(**db_config)
+
     return __mydb
